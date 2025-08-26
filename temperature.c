dd86499147714fe75f97f82e4f1ed9d85a9de004
1harshkashyap <harshkashyap773706@gmail.com>
2025-08-26 16:50:00 +0530

    OK

---
+++ temperature.c
@@ -0,0 +1,9 @@
+//C PROGRAMME FOR AREA OF A RECTANGLE OF GIVEN DIMENSIONS
+#include <stdio.h>
+int main(){
+    float a;
+    printf ("ENTER TEMPERATURE IN DEGREE CELSIUS:");
+    scanf("%f",&a);
+    printf("THE TEPERATURE IN DEGREE FAHRENHEIT IS: %f", (a* 9/5) + 32);
+    return 0;
+}
